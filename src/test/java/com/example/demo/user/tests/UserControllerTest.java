08e6ad1548708eff9dd35d92c619e92e48d3f841
ayoujil200 <ayoujil.200@gmail.com>
2023-03-16 10:43:58 +0100

    feat/TPRest

--- src/test/java/com/example/demo/user/tests/UserControllerTest.java
+++ src/test/java/com/example/demo/user/tests/UserControllerTest.java
@@ -1,11 +1,11 @@
 package com.example.demo.user.tests;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.List;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,11 +28,17 @@ public class UserControllerTest {
 	@MockBean
 	private UserImpl userService;
 
+	private final User user = new User(200L, "f1", "l1", "e1@gmail.com");
+
 	@Test
-	@Order(1)
+	public void contextLoads() throws Exception {
+		assertThat(mockMvc).isNotNull();
+		assertThat(userService).isNotNull();
+	}
+
+	@Test
 	public void shouldCreateUser() throws Exception {
-		User user = new User(200L, "f1", "l1", "e1@gmail.com");
-		when(userService.createOne(any(User.class))).thenReturn(user);
+		when(userService.createOne(user)).thenReturn(user);
 		mockMvc.perform(MockMvcRequestBuilders.post("/users").content(asJsonString(user))
 				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
@@ -43,29 +49,23 @@ public class UserControllerTest {
 	}
 
 	@Test
-	@Order(2)
 	public void shouldDeleteUser() throws Exception {
-		User user = new User(200L, "f1", "l1", "e1@gmail.com");
-		when(userService.deleteOne(any(Long.class))).thenReturn(true);
+		when(userService.deleteOne(user.getId())).thenReturn(true);
 		mockMvc.perform(MockMvcRequestBuilders.delete("/users/" + user.getId()).contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
 	}
 
 	@Test
-	@Order(5)
 	public void shouldUpdateUser() throws Exception {
-		User user = new User(200L, "f1", "l1", "e1@gmail.com");
-		when(userService.updateOne(any(User.class))).thenReturn(true);
+		when(userService.updateOne(user)).thenReturn(true);
 		mockMvc.perform(MockMvcRequestBuilders.put("/users").content(asJsonString(user))
 				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print())
 				.andExpect(status().isOk());
 	}
 
 	@Test
-	@Order(4)
 	public void shouldReturnOneUser() throws Exception {
-		User user = new User(200L, "f1", "l1", "e1@gmail.com");
-		when(userService.getOne(any(long.class))).thenReturn(user);
+		when(userService.getOne(user.getId())).thenReturn(user);
 		mockMvc.perform(MockMvcRequestBuilders.get("/users/" + user.getId()).content(asJsonString(user))
 				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
@@ -76,14 +76,13 @@ public class UserControllerTest {
 	}
 
 	@Test
-	@Order(3)
 	public void shouldReturnListOfUsers() throws Exception {
-		when(userService.getAll()).thenReturn(List.of(new User(null, "f3", "l3", "e3@gmail.com")));
+		when(userService.getAll()).thenReturn(List.of(user));
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/users")).andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(1))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value("e3@gmail.com"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("f3"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value("l3"));
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value(user.getEmail()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value(user.getFirstName()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value(user.getLastName()));
 	}
 
 	public static String asJsonString(final Object obj) {
